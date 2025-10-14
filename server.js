49f3d44f533e4eff16222fa75d4af3c3753c3189
hhh <gyongjun@mail.ru>
2025-10-14 10:17:56 -0700

    server.js 생성

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express.use();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+const routes = require('/router');
+app.use('/', routes);
+
+const port = 3000;
+app.listen(port, () => {
+    console.log("Server running on port %s", port);
+});
